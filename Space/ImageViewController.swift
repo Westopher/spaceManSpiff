bd8118e62a24fbd5eaa7a0be84eda183d5e40cb8
West Kraemer <kraemerwest@gmail.com>
2017-08-03 13:01:19 -0400

    add url for image in imageView

--- Space/ImageViewController.swift
+++ Space/ImageViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ImageViewController: UIViewController {
 
-    var imageURL: URL?
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+            image = UIImage(data: imageData)
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        imageURL = DemoURL.stanford
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
 
 }
